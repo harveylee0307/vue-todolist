53b79c2c3e3c67e88a69564e1144b69663ecb445
Harvey <harveylee0307@gmail.com>
2020-05-24 15:06:23 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import All from '../views/All.vue'
+import Complete from '../views/Complete.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'All',
+    component: All
+  },
+  {
+    path: '/active',
+    name: 'Active',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Active.vue')
+  },
+  {
+    path: '/complete',
+    name: 'Complete',
+    component: Complete
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
